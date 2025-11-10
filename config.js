e5712b88104a541afa3201485e7b9f6c5d3c46e1
virush <mm7466791@gmail.com>
2025-11-10 14:09:24 +0200

    Update Supabase configuration values

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
-export const SUPABASE_URL = "https://supabase.com/dashboard/project/ngtzknecstzlxcpeelth/settings/api-keys";
+export const SUPABASE_URL = "https://ngtzknecstzlxcpeelth.supabase.co";
 export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ndHprbmVjc3R6bHhjcGVlbHRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI2MTQ5NjksImV4cCI6MjA3ODE5MDk2OX0.IXvn2GvftKM96DObzCzA1Nvaye9dHri7t5SZfER0eDg";
 export const BUCKET_NAME = "product-images";
+
 
